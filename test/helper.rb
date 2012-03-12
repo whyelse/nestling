64fa8f220262c7c6793476faad5470068b577411
tzumby <razvan@wisecatmedia.com>
2012-03-12 14:14:56 -0400

    Update the stub_http_get method to return an OpenStruct object that mimics the Net::HTTP response

--- test/helper.rb
+++ test/helper.rb
@@ -2,9 +2,11 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'nestling')
 
 require 'minitest/autorun'
 require 'mocha'
+require 'ostruct'
 
 def stub_http_get(ret)
-  Net::HTTP.any_instance.stubs(:get).returns([{}, ret])
+  response = OpenStruct.new({:body => ret})
+  Net::HTTP.any_instance.stubs(:get).returns(response)
 end
 
 def expect_request(resp, req, opts = nil)
